0a9cc5492c4c839cccf94957bb12ab494d12348c
lixiaofei05 <lixiaofei05@baidu.com>
2026-08-26 19:48:20 +0800

    适配千帆模型与本地Milvus环境：修复Tavily导入依赖，MILVUS_URI改走环境变量

--- llm_models/all_llm.py
+++ llm_models/all_llm.py
@@ -1,7 +1,7 @@
 from langchain_community.tools import TavilySearchResults
 from langchain_openai import ChatOpenAI
 
-from utils.env_utils import OPENAI_API_KEY, DEEPSEEK_API_KEY
+from utils.env_utils import OPENAI_API_KEY, DEEPSEEK_API_KEY, TAVILY_API_KEY
 
 llm = ChatOpenAI(  # 百度千帆
     temperature=0,
@@ -10,7 +10,7 @@ llm = ChatOpenAI(  # 百度千帆
     base_url="https://qianfan.baidubce.com/v2")
 
 
-web_search_tool = TavilySearchResults(max_results=2)
+web_search_tool = TavilySearchResults(max_results=2, tavily_api_key=TAVILY_API_KEY or "placeholder")
 
 # llm = ChatOpenAI(
 #     temperature=0.5,
--- utils/env_utils.py
+++ utils/env_utils.py
@@ -6,6 +6,7 @@ load_dotenv(override=True)
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
+TAVILY_API_KEY = os.getenv('TAVILY_API_KEY')
 
 MILVUS_URI = os.getenv('MILVUS_URI', 'http://127.0.0.1:19530')
 
